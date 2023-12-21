f298215ce163b00ded4816542b933124dc388ebf
Scotsman Dan <daniel.harrold@hotmail.com>
2023-12-21 02:17:55 +0000

    Added network module

--- main.tf
+++ main.tf
@@ -133,10 +133,6 @@ resource "azurerm_linux_virtual_machine" "catapp" {
 
   }
 
-  tags {
-    Department = devops
-    }
-
   # Added to allow destroy to work correctly.
   depends_on = [azurerm_network_interface_security_group_association.catapp-nic-sg-ass]
 }
